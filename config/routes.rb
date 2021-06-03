88a436afbca766c48ae46d36bee40689377ff6ba
ranranluu <ranranluutennis@gmail.com>
2021-06-03 09:44:11 +0000

    [update]トップページのhtmlのみ終了]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'top' => 'books#top'
+
+  root 'books#top'
+
+  get 'books' => 'books#index'
+  
+  resource :books
+
 end
