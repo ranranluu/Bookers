0a6a6c1a2d095fa35bb7df7a0c7f834e114682e5
ranranluu <ranranluutennis@gmail.com>
2021-06-04 01:51:51 +0000

    [update]createアクション追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,8 @@
 class BooksController < ApplicationController
-  
+
   def top
   end
-  
+
   def index
   end
 
@@ -10,9 +10,24 @@ class BooksController < ApplicationController
   end
 
   def new
+    @books = Books.new
   end
 
   def edit
   end
-  
+
+  def create
+    books = Books.new(books_params)
+    books.save
+    redirect_to books_path
+  end
+
+
+
+  private
+  def books_params
+    params.require(:books).permit(:title,:body)
+  end
+
+
 end
